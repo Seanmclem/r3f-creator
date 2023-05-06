61c76c8c1c61d4aa4cf889652eb68743ba1c2815
Seanmclem <seanmclements@gmail.com>
2023-05-06 17:52:18 -0400

    Basic everything wokring for camera

--- exported/src/components/ThirdPersonCharacterE1.tsx
+++ exported/src/components/ThirdPersonCharacterE1.tsx
@@ -4,6 +4,9 @@ import * as THREE from "three";
 import { PerspectiveCamera } from "@react-three/drei";
 import { useControls } from "leva";
 
+const degrees_to_radians = (degrees: number) => degrees * (Math.PI / 180);
+const radians_to_degrees = (radians: number) => radians * (180 / Math.PI);
+
 export const ThirdPersonCharacter = ({ position, rotation }: any) => {
   // MOVEMENT START
   const pressedKeys = useRef(new Set());
@@ -35,10 +38,15 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
 
   const following_mesh_ref = useRef<THREE.Mesh>(null);
 
-  const { cameraDistance, cameraHeight } = useControls({
-    cameraDistance: 3.5,
-    cameraHeight: 2.5,
-  });
+  const prev_position_ref = useRef(position);
+
+  const [{ cameraDistance, cameraHeight, currentAngle, movementAngle }, set] =
+    useControls(() => ({
+      cameraDistance: 3.5,
+      cameraHeight: 2.5,
+      currentAngle: 0,
+      movementAngle: 0,
+    }));
 
   // Use useFrame to update the camera position and lookAt
   useFrame(() => {
@@ -56,6 +64,12 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
     const character = character_ref.current;
     const camera = camera_ref.current;
     const following_mesh = following_mesh_ref.current;
+
+    const before_point = new THREE.Vector3(
+      following_mesh.position.x,
+      following_mesh.position.y,
+      following_mesh.position.z
+    );
 
     // Move forward
     if (pressedKeys.current.has("w")) {
@@ -111,11 +125,19 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
     // Turn character left
     if (pressedKeys.current.has("ArrowLeft")) {
       character.rotation.y += rotationSpeed;
+      // following_mesh.rotation.y += rotationSpeed;
     }
     // Turn character right
     if (pressedKeys.current.has("ArrowRight")) {
       character.rotation.y -= rotationSpeed;
+      // following_mesh.rotation.y -= rotationSpeed;
     }
+
+    const pressing_any_WASD =
+      pressedKeys.current.has("w") ||
+      pressedKeys.current.has("a") ||
+      pressedKeys.current.has("s") ||
+      pressedKeys.current.has("d");
 
     // Update camera position and lookAt
     const cameraPosition = new THREE.Vector3(0, cameraHeight, -cameraDistance);
@@ -132,6 +154,46 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
 
     // Set the camera's lookAt target to the center of the character mesh
     camera.lookAt(characterCenter);
+    // ^ the most important line of code in this file
+    ///
+    //
+
+    // const after_point = new THREE.Vector3(
+    //   following_mesh.position.x,
+    //   following_mesh.position.y,
+    //   following_mesh.position.z
+    // );
+
+    // const deltaX = after_point.x - before_point.x;
+    // const deltaY = after_point.y - before_point.y;
+    // const angle_radians = Math.atan2(deltaY, deltaX);
+    // // const angle_degrees = angle_radians * (180 / Math.PI);
+
+    // const newRotation = new THREE.Euler(
+    //   0, // x-axis rotation
+    //   angle_radians, // y-axis rotation
+    //   0, // z-axis rotation
+    //   "YXZ" // rotation order
+    // );
+
+    if (!pressing_any_WASD) {
+      return;
+    }
+
+    // character_ref.setRotationFromEuler(newRotation);
+
+    const direction = new THREE.Vector3()
+      .copy(following_mesh_ref.current.position)
+      .sub(prev_position_ref.current)
+      .normalize();
+
+    // Rotate the mesh to face the direction of movement
+    following_mesh_ref.current.lookAt(
+      following_mesh_ref.current.position.clone().add(direction)
+    );
+
+    // Update the previous position
+    prev_position_ref.current = following_mesh_ref.current.position.clone();
   });
   return (
     <>
