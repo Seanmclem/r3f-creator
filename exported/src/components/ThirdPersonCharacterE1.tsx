610ddad00230846436592fff186e0ae6ee032537
Seanmclem <seanmclements@gmail.com>
2023-05-09 21:59:20 -0400

    objectives

--- exported/src/components/ThirdPersonCharacterE1.tsx
+++ exported/src/components/ThirdPersonCharacterE1.tsx
@@ -5,17 +5,34 @@ import { PerspectiveCamera } from "@react-three/drei";
 import { useControls } from "leva";
 import { useKeyListeners } from "../hooks/character-hooks";
 
+// Objectives Completed:
+// 1. make the camera follow the character
+// 2. make the character rotate smoothly
+// 3. make the character move smoothly
+// 4. make the character face the direction it is moving
+// 5. make the characters forward direction, and others, be relative to the direction the camera is facing
+
+// Objectives Not Completed:
+// 1. make jumping
+// 2. make the character have a rigid body colisions etc
+// 3. make the character have a custom model and animations
+// 4. use hooks to also listen to controller input
+// 5. hooks for touch screen input
+// 6. unity amoung inputs
+// 7. camera controls need to move up and down
+// 8. camera will one day need colisions and auto adjusting?
+
 export const ThirdPersonCharacter = ({ position, rotation }: any) => {
   // MOVEMENT START
   const pressedKeys = useKeyListeners();
 
-  const character_ref =
+  const follower_mesh_ref =
     useRef<THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>>(
       null
     );
   const camera_ref = useRef<THREE.PerspectiveCamera>(null);
 
-  const following_mesh_ref = useRef<THREE.Mesh>(null);
+  const character_mesh_ref = useRef<THREE.Mesh>(null);
 
   const prev_position_ref = useRef(position);
 
@@ -29,89 +46,96 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
     currentAngle: 0,
     movementAngle: 0,
   }));
+  const previou_speed_ref = useRef(0);
 
   // Use useFrame to update the camera position and lookAt
   useFrame((_, delta) => {
     if (
-      !character_ref.current ||
+      !follower_mesh_ref.current ||
       !camera_ref.current ||
-      !following_mesh_ref.current
+      !character_mesh_ref.current
     ) {
       return;
     }
 
     // console.log("delta", delta);
 
-    const movementSpeed = parseFloat(delta.toFixed(2)) * 9; // Adjust this value to control the character's movement speed
-    // const movementSpeed = 0.1;
-    // console.log("movementSpeed", movementSpeed);
-    // const rotationSpeed = 0.03;
-    const rotationSpeed = parseFloat(delta.toFixed(2)) * 3; // Adjust this value to control the character's rotation speed
-    // console.log("rotationSpeed", rotationSpeed);
+    // const good_delta = delta === 0 ? - previou_delta_ref.current;
+    const speed_calc = parseFloat(delta.toFixed(2)) * 9;
+    previou_speed_ref.current =
+      speed_calc === 0 ? previou_speed_ref.current : speed_calc;
 
-    const character = character_ref.current;
+    const movementSpeed = previou_speed_ref.current; // Adjust this value to control the character's movement speed
+    // const movementSpeed = 0.1;
+    console.log("movementSpeed", movementSpeed);
+    console.log("delta", delta);
+    // const rotationSpeed = 0.03;
+    const rotationSpeed = previou_speed_ref.current / 3; // Adjust this value to control the character's rotation speed
+    console.log("rotationSpeed", rotationSpeed);
+
+    const follower_current = follower_mesh_ref.current;
     const camera = camera_ref.current;
-    const following_mesh = following_mesh_ref.current;
+    const character_current = character_mesh_ref.current;
 
     // Move forward
     if (pressedKeys.current.has("w")) {
-      const z_change = Math.cos(character.rotation.y) * movementSpeed;
-      const x_change = Math.sin(character.rotation.y) * movementSpeed;
+      const z_change = Math.cos(follower_current.rotation.y) * movementSpeed;
+      const x_change = Math.sin(follower_current.rotation.y) * movementSpeed;
 
-      character.position.z += z_change;
-      character.position.x += x_change;
+      follower_current.position.z += z_change;
+      follower_current.position.x += x_change;
 
-      following_mesh.position.z += z_change;
-      following_mesh.position.x += x_change;
+      character_current.position.z += z_change;
+      character_current.position.x += x_change;
     }
     // Move backward
     if (pressedKeys.current.has("s")) {
-      const z_change = Math.cos(character.rotation.y) * movementSpeed;
-      const x_change = Math.sin(character.rotation.y) * movementSpeed;
+      const z_change = Math.cos(follower_current.rotation.y) * movementSpeed;
+      const x_change = Math.sin(follower_current.rotation.y) * movementSpeed;
 
-      character.position.z -= z_change;
-      character.position.x -= x_change;
+      follower_current.position.z -= z_change;
+      follower_current.position.x -= x_change;
 
-      following_mesh.position.z -= z_change;
-      following_mesh.position.x -= x_change;
+      character_current.position.z -= z_change;
+      character_current.position.x -= x_change;
     }
     // Move left
     if (pressedKeys.current.has("a")) {
-      const z_change = Math.sin(character.rotation.y) * movementSpeed;
-      const x_change = Math.cos(character.rotation.y) * movementSpeed;
+      const z_change = Math.sin(follower_current.rotation.y) * movementSpeed;
+      const x_change = Math.cos(follower_current.rotation.y) * movementSpeed;
 
-      character.position.z -= z_change;
-      character.position.x += x_change;
+      follower_current.position.z -= z_change;
+      follower_current.position.x += x_change;
 
-      following_mesh.position.z -= z_change;
-      following_mesh.position.x += x_change;
+      character_current.position.z -= z_change;
+      character_current.position.x += x_change;
     }
     // Move right
     if (pressedKeys.current.has("d")) {
-      const z_change = Math.sin(character.rotation.y) * movementSpeed;
-      const x_change = Math.cos(character.rotation.y) * movementSpeed;
+      const z_change = Math.sin(follower_current.rotation.y) * movementSpeed;
+      const x_change = Math.cos(follower_current.rotation.y) * movementSpeed;
 
-      character.position.z += z_change;
-      character.position.x -= x_change;
+      follower_current.position.z += z_change;
+      follower_current.position.x -= x_change;
 
-      following_mesh.position.z += z_change;
-      following_mesh.position.x -= x_change;
+      character_current.position.z += z_change;
+      character_current.position.x -= x_change;
     }
 
     // now make heirght position match
-    following_mesh.position.y = character.position.y + 1;
+    character_current.position.y = follower_current.position.y + 1;
 
     //   follower_box_ref.position.y =
     // character_mesh_ref.position.y + follower_height;
 
     // Turn character left
     if (pressedKeys.current.has("ArrowLeft")) {
-      character.rotation.y += rotationSpeed;
+      follower_current.rotation.y += rotationSpeed;
       // following_mesh.rotation.y += rotationSpeed;
     }
     // Turn character right
     if (pressedKeys.current.has("ArrowRight")) {
-      character.rotation.y -= rotationSpeed;
+      follower_current.rotation.y -= rotationSpeed;
       // following_mesh.rotation.y -= rotationSpeed;
     }
 
@@ -123,16 +147,16 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
 
     // Update camera position and lookAt
     const cameraPosition = new THREE.Vector3(0, cameraHeight, -cameraDistance);
-    cameraPosition.applyQuaternion(character.quaternion);
-    cameraPosition.add(character.position);
+    cameraPosition.applyQuaternion(follower_current.quaternion);
+    cameraPosition.add(follower_current.position);
     camera.position.copy(cameraPosition);
 
     // Calculate the center of the character mesh
     const characterCenter = new THREE.Vector3();
-    character.geometry.computeBoundingBox();
-    character.geometry.boundingBox &&
-      character.geometry.boundingBox.getCenter(characterCenter);
-    characterCenter.applyMatrix4(character.matrixWorld);
+    follower_current.geometry.computeBoundingBox();
+    follower_current.geometry.boundingBox &&
+      follower_current.geometry.boundingBox.getCenter(characterCenter);
+    characterCenter.applyMatrix4(follower_current.matrixWorld);
 
     // Set the camera's lookAt target to the center of the character mesh
     camera.lookAt(characterCenter);
@@ -149,7 +173,7 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
     // character_ref.setRotationFromEuler(newRotation);
 
     const direction = new THREE.Vector3()
-      .copy(following_mesh_ref.current.position)
+      .copy(character_mesh_ref.current.position)
       .sub(prev_position_ref.current)
       .normalize();
 
@@ -164,14 +188,14 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
       // Interpolate the rotation
       currentRotationRef.current.slerp(targetRotationRef.current, 0.1);
 
-      following_mesh_ref.current.quaternion.copy(currentRotationRef.current);
+      character_mesh_ref.current.quaternion.copy(currentRotationRef.current);
     } else {
       // Set the target rotation to the current rotation without tweening
-      following_mesh_ref.current.quaternion.copy(targetRotationRef.current);
+      character_mesh_ref.current.quaternion.copy(targetRotationRef.current);
       currentRotationRef.current.copy(targetRotationRef.current);
     }
 
-    prev_position_ref.current = following_mesh_ref.current.position.clone();
+    prev_position_ref.current = character_mesh_ref.current.position.clone();
     // smooth rotation end
   });
   return (
@@ -185,13 +209,13 @@ export const ThirdPersonCharacter = ({ position, rotation }: any) => {
           // matrixWorldAutoUpdate={undefined}
           // getObjectsByProperty={undefined}
         />
-        <mesh ref={character_ref}>
+        <mesh ref={follower_mesh_ref}>
           <boxGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
           <meshBasicMaterial color="green" wireframe={true} />
         </mesh>
       </group>
 
-      <mesh ref={following_mesh_ref}>
+      <mesh ref={character_mesh_ref}>
         <boxGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
         <meshBasicMaterial attach="material" color="orange" />
         <arrowHelper args={[new THREE.Vector3(0, 0, 1)]} />
